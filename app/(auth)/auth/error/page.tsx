398379164a7f59972103750d7e8831b594f879a8
Danng <futuretech.coder4@gmail.com>
2026-08-26 10:44:45 -0500

    Add suspense

--- app/(auth)/auth/error/page.tsx
+++ app/(auth)/auth/error/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { authClient } from "@/lib/auth-client";
-import { useEffect, useState } from "react";
+import {Suspense, useEffect, useState} from "react";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Gavel, HammerIcon} from "lucide-react";
 
@@ -29,19 +29,52 @@ export default function AuthErrorPage() {
 
     if (code === "BANNED_USER") {
         if (!loaded) {
-            return <div><p>Loading...</p></div>;
+            return <Suspense>
+                <div><p>Loading...</p></div>
+            </Suspense>;
         }
 
         if (expires === undefined) {
             return (
-                <div>
-                    <p>Auth error</p>
-                    <p>Code: {code}</p>
-                    <p>{message}</p>
-                </div>
+                <Suspense>
+                    <div>
+                        <p>Auth error</p>
+                        <p>Code: {code}</p>
+                        <p>{message}</p>
+                    </div>
+                </Suspense>
             );
         }
         return (
+            <Suspense>
+                <div
+                    className="flex flex-col relative max-w w-full p-10 text-center overflow-hidden min-h-screen h-full justify-center"
+                    style={{
+                        backgroundColor: "#0a0e17",
+                        backgroundImage: "url('/stardust.png')",
+                        backgroundRepeat: "repeat",
+                    }}
+                >
+                    <Card
+                        size={"default"}
+                        className="flex flex-col justify-center items-center mx-auto w-full max-w-md md:w-3/12 min-h-[220px] bg-secondary rounded-xl text-slate-400 p-8"
+                    >
+                        <CardHeader className="items-center justify-center">
+                            <Gavel color="#bd0a0a" />
+                            <CardTitle>Your account has been suspended</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <p>Reason: {banReason}</p>
+                            {expires === null? <div></div> : <p>Expires on {expires.toDateString()}</p>}
+                        </CardContent>
+                    </Card>
+                </div>
+            </Suspense>
+        );
+    }
+
+    return (
+        <Suspense>
             <div
                 className="flex flex-col relative max-w w-full p-10 text-center overflow-hidden min-h-screen h-full justify-center"
                 style={{
@@ -56,38 +89,13 @@ export default function AuthErrorPage() {
                 >
                     <CardHeader className="items-center justify-center">
                         <Gavel color="#bd0a0a" />
-                        <CardTitle>Your account has been suspended</CardTitle>
+                        <CardTitle>Auth Error</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        <p>Reason: {banReason}</p>
-                        {expires === null? <div></div> : <p>Expires on {expires.toDateString()}</p>}
+                        <p>{message}</p>
                     </CardContent>
                 </Card>
             </div>
-        );
-    }
-
-    return (
-        <div
-            className="flex flex-col relative max-w w-full p-10 text-center overflow-hidden min-h-screen h-full justify-center"
-            style={{
-                backgroundColor: "#0a0e17",
-                backgroundImage: "url('/stardust.png')",
-                backgroundRepeat: "repeat",
-            }}
-        >
-            <Card
-                size={"default"}
-                className="flex flex-col justify-center items-center mx-auto w-full max-w-md md:w-3/12 min-h-[220px] bg-secondary rounded-xl text-slate-400 p-8"
-            >
-                <CardHeader className="items-center justify-center">
-                    <Gavel color="#bd0a0a" />
-                    <CardTitle>Auth Error</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>{message}</p>
-                </CardContent>
-            </Card>
-        </div>
+        </Suspense>
     );
 }
